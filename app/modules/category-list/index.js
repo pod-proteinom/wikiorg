95f1446a038e9c4926d6486b191960871f8975c2
pod-proteinom <alta0815@yandex.ru>
2016-10-08 16:44:59 +0300

    Add category and city lists

---
+++ app/modules/category-list/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = ['sluzhby', 'avto', 'gorod-vlast', 'dosug',
+  'tekhnologii', 'uslugi', 'tekhnika', 'kultura', 'mebel',
+  'zdorove', 'khimiya', 'oborudovanie', 'obrazovanie', 'odezhda',
+  'bezopasnost', 'produkty-pitaniya', 'reklama', 'otdykh', 'stroitelnye-materialy',
+  'nedvizhimost', 'interer', 'veterinariya', 'magaziny', 'transport',
+  'khoztovary', 'ehlektronika', 'yur-uslugi'
+];
