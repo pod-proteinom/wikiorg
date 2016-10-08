588885c91c2413a9186ee5536cc185397e65a8f4
pod-proteinom <alta0815@yandex.ru>
2016-10-09 01:21:22 +0300

    Initialize volume with data

--- volume/init.sh
+++ volume/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-if [ -z "$1" ] || [ -z "$2" ]; then
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
 	exit -1
 else
 	VOLUME="data"
@@ -9,7 +9,23 @@ fi
 
 if !(docker volume ls | grep $VOLUME &>/dev/null;)
 then
-	docker run -v $VOLUME:/data/db --name mongod -d mongo --directoryperdb
+	docker run -v $VOLUME:/data/db --name mongod -d mongo --directoryperdb	
+	docker cp data/ mongod:/
+	for dir in $3 ; do
+		if [[ -d $dir ]]; then
+			for city in $dir* ; do
+				if [[ -d $city ]]; then
+					for file in $city/*.json ; do
+						db=${city##*/}
+						echo $db
+						filename=${file##*/}
+						withoutExt=${filename%.*}
+						docker exec mongod mongoimport -d "$db" -c "$withoutExt" /"$file"
+					done
+				fi
+			done
+		fi
+	done
 	docker exec mongod mongo admin --eval "db.createUser({user: '$USER', pwd: '$PWD', roles: [ 'root' ]})"
 	docker stop mongod
 	docker rm mongod
