6361c05b2181425018c629c0c42cd072d68ffe47
pod-proteinom <alta0815@yandex.ru>
2016-10-08 17:12:41 +0300

    Set pug as default view engine and specify viewsDir

--- app/modules/app/index.js
+++ app/modules/app/index.js
@@ -3,10 +3,15 @@ const conf = require('config');
 const join = require('path').join;
 
 const handlers = require('modules/handlers').handlers;
+const viewDirs = require('modules/handlers').viewDirs;
 const importHandler = require('modules/handlers').importHandler;
 
 const express = require('express');
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', viewDirs);
+// app.locals.basedir = join(conf.server.rootDir, 'views');
 
 handlers.forEach(handler => importHandler(handler, app));
 
--- app/modules/handlers/index.js
+++ app/modules/handlers/index.js
@@ -3,15 +3,19 @@ const conf = require('config');
 const join = require('path').join;
 
 const middlewares = [
-    'middlewares/log-req'
+    'middlewares/00-log-req'
 ];
 
 const api = [
-	'api/0-home'
+	'api/0-home',
+	'api/6-errors'
 ];
 
 const handlers = middlewares.concat(api);
+const viewDirs = api.map(api => join(conf.server.rootDir, 'api', api, 'view'));
 
 exports.importHandler = require('./lib/import-handler');
 
 exports.handlers = handlers;
+
+exports.viewDirs = viewDirs;
