b00cb912902bf357c48366828cc01c11b8ab0e3a
pod-proteinom <alta0815@yandex.ru>
2016-10-09 23:04:32 +0300

    Implement city endpoint

--- app/api/2-category/router.js
+++ app/api/2-category/router.js
@@ -3,27 +3,32 @@
 const express = require('express');
 const router = express.Router();
 
-// const db = require('dbConnect');
-// const citiesList = require('citiesList');
-// const categoriesList = require('categoriesList');
+const db = require('modules/connect-db');
+const citiesList = require('modules/city-list');
+const categoriesList = require('modules/category-list');
 
-// router.get('/:city/:category', (req, res, next) => {
-//     const city = req.params.city;
-//     const category = req.params.category;
+router.param('category', (req, res, next, category) => {
+    if (!categoriesList.includes(req.params.category)) {
+        return next(new Error("Unfortunately our service coudn't provide any info about this category"));
+    }
+    return next();
+});
 
-//     if (citiesList.includes(city) && categoriesList.includes(category)) {
-//         const cityDb = db.useDb(city);
-//         cityDb.model('category').findOne({ alias: category }).exec().then(category => {
-//             if (category) {
-//                 const city = category.city.toObject();
-//                 res.render('categories', { category, city });
-//             } else {
-//                 // TODO return page there aren't any undercategories in this category
-//             }
-//         }).catch(next);
-//     } else {
-//         next();
-//     }
-// });
+router.get('/:city/:category', (req, res, next) => {
+    const cityAlias = req.params.city;
+    const categoryAlias = req.params.category;
+
+    const cityDb = db.useDb(cityAlias);
+    cityDb.model('category').findOne({ alias: categoryAlias }).exec().then(category => {
+        if (category) {
+            const city = category.city.toObject();
+            // TODO separate category info and rubrics
+            // const rubrics = category.rubrics;
+            res.render('category', { category, city });
+        } else {
+            // TODO return page there aren't any undercategories in this category
+        }
+    }).catch(next);
+});
 
 module.exports = router;
