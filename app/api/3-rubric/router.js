aa7e0f82f7a55d0d2a79a2432940de4769ab4d12
pod-proteinom <alta0815@yandex.ru>
2016-10-13 23:10:45 +0300

    Add router for rubric endpoint

--- app/api/3-rubric/router.js
+++ app/api/3-rubric/router.js
@@ -1,60 +1,58 @@
 'use strict';
 
-const express = require('express');
-const router = express.Router();
+const db = require('modules/connect-db');
+const router = require('modules/app-router');
 
-// const db = require('connect-db');
-// const citiesList = require('city-list');
-// const categoriesList = require('category-list');
+const Promise = require("bluebird");
+const generatePagination = require('./lib/pagination');
 
-// const Promise = require("bluebird");
-// const generatePagination = require('./lib/pagination');
+router.get('/:city/:category/:rubric', (req, res, next) => next('route'));
 
-// router.get('/:city/:category/:rubric', (req, res, next) => next('route'));
+router.get('/:city/:category/:rubric/:page([0-9]*)?', (req, res, next) => {
+    const url = req.originalUrl;
+    if (req.params.page == 1) {
+        return res.redirect(url.slice(0, url.lastIndexOf('/')));
+    }
 
-// router.get('/:city/:category/:rubric/:page([0-9]*)?', (req, res, next) => {
-//     const url = req.originalUrl;
-//     if (req.params.page == 1) {
-//         return res.redirect(url.slice(0, url.lastIndexOf('/')));
-//     }
+    const PER_PAGE = 15;
+    const city = req.params.city;
+    const category = req.params.category;
+    const rubric = req.params.rubric;
+    const currentPage = parseInt(req.params.page) || 1;
 
-//     const PER_PAGE = 15;
-//     const city = req.params.city;
-//     const category = req.params.category;
-//     const rubric = req.params.rubric;
-//     const currentPage = parseInt(req.params.page) || 1;
-
-//     if (currentPage > 0 && citiesList.includes(city) && categoriesList.includes(category)) {
-//         const cityDb = db.useDb(city);
-//         Promise.props({
-//             orgsCount: cityDb.model(category).find().where('rubrics.alias').in(rubric).count().exec(),
-//             orgs: cityDb.model(category).find().where('rubrics.alias').in(rubric)
-//                 .skip(PER_PAGE * (currentPage - 1)).limit(PER_PAGE).exec()
-//         }).then(result => {
-//             const orgs = result.orgs;
-//             const pagesCount = Math.ceil(result.orgsCount / PER_PAGE);
-//             if (orgs.length) {
-//                 const firstOrg = orgs[0];
-//                 const city = firstOrg.city;
-//                 const getCategory = firstOrg.getCategory();
-//                 const getRubric = firstOrg.getRubric(rubric);
-//                 const pages = generatePagination(currentPage, pagesCount);
-//                 res.render('rubrics', {
-//                     orgs,
-//                     city,
-//                     pages,
-//                     currentPage,
-//                     category: getCategory,
-//                     rubric: getRubric
-//                 });
-//             } else {
-//                 //todo render that there aren't any organisation here
-//                 res.json('there aren\'t any organisation here');
-//             }
-//         }).catch(next);
-//     } else {
-//         next();
-//     }
-// });
+    if (currentPage > 0) {
+        const cityDb = db.useDb(city);
+        Promise.props({
+            orgsCount: cityDb.model(category).find().where('rubrics.alias').in(rubric).count().exec(),
+            orgs: cityDb.model(category).find().where('rubrics.alias').in(rubric)
+                .skip(PER_PAGE * (currentPage - 1)).limit(PER_PAGE).exec()
+        }).then(result => {
+            const orgs = result.orgs;
+            const pagesCount = Math.ceil(result.orgsCount / PER_PAGE);
+            if (orgs.length) {
+                const firstOrg = orgs[0];
+                const city = firstOrg.city;
+                const getCategory = firstOrg.getCategory();
+                const getRubric = firstOrg.getRubric(rubric);
+                const pages = generatePagination(currentPage, pagesCount);
+                console.log(getRubric)
+                console.log(getCategory)
+                res.render('rubrics', {
+                    orgs,
+                    city,
+                    pages,
+                    currentPage,
+                    category: getCategory,
+                    rubric: getRubric
+                });
+            } else {
+                //todo render that there aren't any organisation here
+                res.json('there aren\'t any organisation here');
+            }
+        }).catch(next);
+    } else {
+        next();
+    }
+});
 
 module.exports = router;
