db36ef4640dc353fd82d6dfed17cd39afee4601e
pod-proteinom <alta0815@yandex.ru>
2016-10-13 23:19:02 +0300

    Add router for org endpoint

--- app/api/4-org/router.js
+++ app/api/4-org/router.js
@@ -1,31 +1,25 @@
 'use strict';
 
-const express = require('express');
-const router = express.Router();
+const db = require('modules/connect-db');
+const router = require('modules/app-router');
 
-// const rubricUtil = require('category').rubricUtil;
+router.get('/:city/:category/:rubric/:orgName', (req, res, next) => {
+    const city = req.params.city;
+    const category = req.params.category;
+    const orgName = req.params.orgName;
+    const rubric = req.params.rubric;
 
-// router.get('/:city/:category/:rubric/:orgName', (req, res, next) => {
-// 	const city = req.params.city;
-// 	const category = req.params.category;
-// 	const orgName = req.params.orgName;
-// 	const rubric = req.params.rubric;
-
-// 	if(citiesList.includes(city) && categoriesList.includes(category)) {
-// 		const cityDb = db.useDb(city);
-// 		cityDb.model(category).findOne({alias: orgName}).exec().then(org => {
-// 			if(org) {
-// 				const city = org.city;
-// 				const category = org.getCategory();
-// 				const getRubric = org.getRubric(rubric);
-// 				res.render('organisation', {org, city, category, rubric : getRubric});
-// 			} else {
-// 				return next();
-// 			}
-// 		}).catch(next);
-// 	} else {
-// 		next();
-// 	}
-// });
+    const cityDb = db.useDb(city);
+    cityDb.model(category).findOne({ alias: orgName }).exec().then(org => {
+        if (org) {
+            const city = org.city;
+            const category = org.getCategory();
+            const getRubric = org.getRubric(rubric);
+            res.render('org', { org, city, category, rubric: getRubric });
+        } else {
+            return next();
+        }
+    }).catch(next);
+});
 
 module.exports = router;
