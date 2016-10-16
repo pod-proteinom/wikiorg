8f9dd4c1e2d8908e4e4928b54fdc60340572696b
pod-proteinom <alta0815@yandex.ru>
2016-10-16 19:49:53 +0300

    Fix error handling

--- app/api/6-errors/index.js
+++ app/api/6-errors/index.js
@@ -1,14 +1,23 @@
 'use strict';
 
-exports.init = (app) => {
-	app.use((err, req, res, next) => {
-		console.log(err)
-		res.status(500);
-		res.render('500');
-	});
+const logger = require('modules/logger');
+const ParamsError = require('modules/error');
 
-	app.use((req, res) => {
-		res.status(404);
-		res.render('404');
-	});
+exports.init = (app) => {
+    if (process.env.NODE_ENV == 'production') {
+        app.use((err, req, res, next) => {
+            if (err instanceof ParamsError) {
+                return next();
+            }
+            
+            logger.error(err.message);
+            res.status(500);
+            res.render('500');
+        });
+    }
+
+    app.use((req, res) => {
+        res.status(404);
+        res.render('404');
+    });
 };
