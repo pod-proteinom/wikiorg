5ef423a2d62d3d7c17a9b5e57483f1197b890392
pod-proteinom <alta0815@yandex.ru>
2016-10-16 17:10:05 +0300

    Create basic prod configuration

---
+++ config/production.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const path = require('path');
+const join = path.join;
+const dirname = path.dirname;
+const rootDir = dirname(__dirname);
+const srcDir = join(rootDir, 'app');
+
+module.exports = {
+    server : {
+        port: '4000',
+        srcDir
+    },
+    mongoose: {
+        uri: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@db:27017/`,
+        options: {
+            server: {
+                socketOptions: {
+                    keepAlive: 1
+                },
+                poolSize: 5
+            }
+        }
+    },
+    logger: {
+        level: 'error',
+		colorize: true
+    }
+}
